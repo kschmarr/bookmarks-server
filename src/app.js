d0092a6fca923c037d99c480260f5163eda3f8b4
Kris Schmarr <kschmarr@gmail.com>
2019-05-20 14:15:16 -0600

    second commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
+const { NODE_ENV, API_TOKEN } = require("./config");
+const bmRouter = require("./bm-router");
 
 const app = express();
 
@@ -12,6 +13,17 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.use(bmRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development"
+  NODE_ENV: process.env.NODE_ENV || "development",
+  API_TOKEN: process.env.API_TOKEN || "560b8136-77f7-11e9-8f9e-2a86e4085a59"
 };
